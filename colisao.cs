df37cfdd232956bc4d591608f9f7943109481887
Giovana Stefani <101645617+GsTeti@users.noreply.github.com>
2023-06-14 04:23:28 -0300

    Add files via upload

---
+++ colisao.cs
@@ -0,0 +1,22 @@
+public class Tin : MonoBehaviour, IShotHit {
+
+    new Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void IShotHit.Hit(Vector3 direction)
+    {
+
+        rigidbody.AddForce(Vector3.Scale(direction, new Vector3(50, 100, 50)));
+
+    }
+
+}
